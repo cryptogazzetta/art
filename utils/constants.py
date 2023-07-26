8cdfe0f25dd04be73a571b1f47324589ee8010fc
cryptogazzetta <41456979+cryptogazzetta@users.noreply.github.com>
2023-07-26 15:05:03 -0300

    Create constants.py

---
+++ utils/constants.py
@@ -0,0 +1,12 @@
+# Artsy API constants
+ARTSY_API_URL = 'https://api.artsy.net/api/'
+ARTSY_API_HEADERS = {
+        'Accept': 'application/vnd.artsy-v2+json',
+        'X-Xapp-Token': 'eyJhbGciOiJIUzI1NiJ9.eyJyb2xlcyI6IiIsInN1YmplY3RfYXBwbGljYXRpb24iOiJhNjlkYTRkYi05MTUxLTQ3YTItYjgwNy1hZTU5ODZhNjg2YjIiLCJleHAiOjE2ODk3MDE2MTEsImlhdCI6MTY4OTA5NjgxMSwiYXVkIjoiYTY5ZGE0ZGItOTE1MS00N2EyLWI4MDctYWU1OTg2YTY4NmIyIiwiaXNzIjoiR3Jhdml0eSIsImp0aSI6IjY0YWQ5MjZiNmE3OTgxMDAwYzBlZTM4OCJ9.sr5s8MkGEqr-HZhw5QNNGYN6x9hBUZM36O0BvuXxaQw' 
+    }
+
+# Sotheby's constants
+EMAIL = "christian@fynt.io"
+SOTHEBYS_PASSWORD = "t#3u!%h9Up!JSZb"
+
+OPENAI_API_KEY = "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"
