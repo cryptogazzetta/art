2be7410213dc2a53a0576b2e903907c3e6bdd1aa
cryptogazzetta <41456979+cryptogazzetta@users.noreply.github.com>
2023-07-27 10:00:08 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from extractors import saatchi_artworks, saatchi_artists, artsy
 from infra import gcp
 
-# saatchi_artworks.get_all_artworks_info('art_data_files', 'saatchi_artworks_links.txt', 'saatchi_artworks_info1.csv', 'saatchi_failed_artworks_urls.json')
+saatchi_artworks.get_all_artworks_info('art_data_files', 'saatchi_artworks_links.txt', 'saatchi_artworks_info1.csv', 'saatchi_failed_artworks_urls.json')
 
 # gcp.retrieve_file_from_gcs('art_data_files', 'saatchi_artworks_info.csv', 'saatchi_artworks_info.csv')
 
 # saatchi_artists.get_all_artists_info('art_data_files', 'saatchi_artists_info.json', 'saatchi_artworks_info.csv')
 
-artsy.extract_artsy_data('artworks')
+# artsy.extract_artsy_data('artworks')
